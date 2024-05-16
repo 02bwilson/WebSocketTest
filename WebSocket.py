2ce1dbc0468fa52398469d82259719536a4ed2ca
Bryce W <58643544+02bwilson@users.noreply.github.com>
2024-05-16 22:49:31 +0000

    .

--- WebSocket.py
+++ WebSocket.py
@@ -4,15 +4,30 @@ import asyncio
 import random
 
 from scipy.fft import fft
+import numpy as np
 
 from websockets.server import serve
-
+m = 1
 async def echo(websocket):
     async for message in websocket:
-        data = [random.random() for _ in range(1024)]
+        global m
+        if message.startswith("SF"):
+
+            freq = message[2:]
+            freq = int(freq)
+            m = freq
+            print(freq)
+            # TUNE
+
+
+        # Begin to build the data
+        data = np.array([random.random() for _ in range(1024)])
         data = fft(data)
-        data = [abs(x) for x in data]
+
+        data = [abs(x) + m for x in data]
         data = data[1:]
+
+
         await websocket.send(str(list(data)))
 
 async def main():
